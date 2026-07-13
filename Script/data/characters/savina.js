d6cdef17ae697a51c9468dff513dbaca058fda0d
Satvir Singh <satvir.singh@laservideo.local>
2026-07-13 08:08:21 +0200

    Aggiustato dati savina al volo

--- Script/data/characters/savina.js
+++ Script/data/characters/savina.js
@@ -1,2 +1,2 @@
 // ===== SAVINA F1 =====
-const CHAR_SAVINA = { nome: "Savina F1", hpBase: 5, atkBase: 4, defBase: 7, atkSpec: 4, defSpec: 6, velBase: 4, raritaTipo: "raro", elemento: "terra", immagine: "../Sprite/personaggi/SavinaF1/SavinaF1.jpeg", immagineAtk: "../Sprite/personaggi/SavinaF1/SavinaF1_atk.jpeg", mossaLvl1: "Attacco 1", mossaLvl2: "Attacco 2", mossaLvl3: "Attacco 3", boss: false };
+const CHAR_SAVINA = { nome: "Savina", hpBase: 5, atkBase: 4, defBase: 7, atkSpec: 4, defSpec: 6, velBase: 4, raritaTipo: "raro", elemento: "terra", immagine: "../Sprite/personaggi/Savina/Savina.jpeg", immagineAtk: "../Sprite/personaggi/Savina/Savina_atk.jpeg", mossaLvl1: "Attacco 1", mossaLvl2: "Attacco 2", mossaLvl3: "Attacco 3", boss: false };
