6e691776a27fe863aca7c1372cc6914c4d9f00ee
Satvir Singh <satvir.singh@laservideo.local>
2026-07-17 12:03:57 +0200

    Rifiniture 0.0.5

---
+++ Script/data/characters/re_merlino.js
@@ -0,0 +1,2 @@
+// ===== RE E MERLINO =====
+const CHAR_RE_MERLINO = { nome: "Re e Merlino", hpBase: 10, atkBase: 4, defBase: 5, atkSpec: 5, defSpec: 7, velBase: 5, raritaTipo: "epico", elemento: "terra", immagine: "../Sprite/personaggi/Re e Merlino/Re_Merlino.jpeg", immagineAtk: "../Sprite/personaggi/Re e Merlino/Re_Merlino_atk.jpeg", mossaLvl1: "Attacco 1", mossaLvl2: "Attacco 2", mossaLvl3: "|", boss: false };
